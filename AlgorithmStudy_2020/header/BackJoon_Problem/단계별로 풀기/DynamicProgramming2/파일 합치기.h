7d3b6b8f321031acdd9cfb6bf21b008f07b2070a
DonghyunKim98 <akainoo@naver.com>
2020-07-11 22:12:57 +0900

    7.11 - Dynamic Programming
    DP : 11066

---
+++ AlgorithmStudy_2020/header/BackJoon_Problem/단계별로 풀기/DynamicProgramming2/파일 합치기.h
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+//11066¹ø ¹®Á¦
+void FileMerge() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int T;
+	cin >> T;
+	while (T > 0) {
+		int K;
+		cin >> K;
+		vector<int> vc(K);
+		for (int i = 0; i < K; i++) 
+			cin >> vc[i];
+		
+		T--;
+	}
+}
