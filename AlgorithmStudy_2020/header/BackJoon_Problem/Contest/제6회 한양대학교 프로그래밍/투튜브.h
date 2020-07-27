47e1b60731ca20f9b930b901882c04296b589134
DonghyunKim98 <akainoo@naver.com>
2020-07-27 17:40:20 +0900

    7.27 - 제 6회 한양대학교 프로그래밍 경시대회
    제 6회 한양대학교 프로그래밍 경시대회 : 17954

--- AlgorithmStudy_2020/header/BackJoon_Problem/Contest/제6회 한양대학교 프로그래밍/투튜브.h
+++ AlgorithmStudy_2020/header/BackJoon_Problem/Contest/제6회 한양대학교 프로그래밍/투튜브.h
@@ -6,51 +6,51 @@ void solution() {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	int N;
 	cin >> N;
-	int answer = 0, total = (2 * N) * (2 * N + 1) / 2;
-
+	int** arr = new int* [2];
+	for (int i = 0; i < 2; i++)
+		arr[i] = new int[N];
 	/*
 	ex) n=5
 	[10 1 2 3 9]
 	[8  4 5 6 7]
 	=> 7 6 5 4 8 9 3 2 1 10
 	*/
-	//0초
-	total -= (2 * N) - 3;
-	//1초~n-2초
-	for (int i = (2 * N - 4); i >= N - 1; i--) {
-		answer += total * (i - 2 * N + 5);
-		total -= i;
-	}
-	//n-1초
-	answer += total * (N - 1);
-	total -= (2 * N) - 2;
-	//n초
-	answer += total * N;
-	total -= (2 * N) - 1;
-	//n+1초~2*n-2초
-	for (int i = N - 2; i >= 1; i--) {
-		answer += total * (2 * N - i - 1);
-		total -= i;
-	}
-	//2*n-1초
-	answer += total * (2 * N - 1);
-	total -= 2 * N;
+	//첫번째 줄 입력
+	arr[0][0] = 2 * N;
+	arr[0][N - 1] = 2 * N - 1;
+	for (int i = 1; i < N - 1; i++)
+		arr[0][i] = i;
+	//두번째 줄 입력
+	arr[1][0] = 2 * N - 2;
+	arr[1][N - 1] = 2 * N - 3;
+	for (int i = 1; i < N - 1; i++)
+		arr[1][i] = (N - 2) + i;
 
-	cout << answer << '\n';
-	cout << 2 * N << ' ';
-	for (int i = 1; i <= N - 2; i++)
-		cout << i << ' ';
-	cout << 2 * N - 1 << '\n';
-	cout << 2 * N - 2 << ' ';
-	for (int i = N - 1; i <= 2 * N - 4; i++)
-		cout << i << ' ';
-	cout << 2 * N - 3;
+	long long answer = 0, total = (2 * N) * (2 * N + 1) / 2, day = -1;
+	//계산
+	//0번째 날~N-1번째 날
+	for (int i = 1; i >= 0; i--) {
+		for (int j = N - 1; j >= 0; j--) {
+			day++;
+			answer += total * day;
+			total -= arr[i][j];
+		}
+	}
+	//N이 1일때 예외처리
+	if (N == 1) {
+		cout << 2 << '\n';
+		cout << 1 << '\n';
+		cout << 2 << '\n';
+	}
+	else {
+		cout << answer << '\n';
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < N; j++) {
+				cout << arr[i][j] << ' ';
+			}
+			cout << '\n';
+		}
+	}
 }
 
 
-/*
-	ex) n=5
-	[10 1 2 3 9]
-	[8  4 5 6 7]
-	=> 7 6 5 4 8 9 3 2 1 10
-*/
---
+++ AlgorithmStudy_2020/header/BackJoon_Problem/삼성 SW 역량 테스트 기출 문제/구슬 탈출 2.h
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+//13460번 문제
+using namespace std;
+const int MAX = 10;
+int N, M;
+pair<int, int> red, blue, hole;
+int board[10][10];
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,-1,0,1 };
+int MAX_CNT = 0, answer = INT_MAX;
+void DFS(int Rx, int Ry, int Bx, int By, int cnt) {
+	if (cnt > 10) answer = -1;
+
+	for (int k = 0; k < 4; k++) {
+		int nRx = Rx, nRy = Ry, nBx = Bx, nBy = By, Rcnt = 0, Bcnt = 0;
+		bool is_R_hole = false, is_B_hole = false;
+		cnt ++;
+		//빨간 구슬 굴리기
+		while (board[Ry + dy[k]][Rx + dx[k]]) {
+			if (Ry + dy[k] >= 0 && Ry + dy[k] < N && Rx + dx[k] >= 0 && Rx + dx[k] < M) {
+				nRx += dx[k], nRy +=  dy[k];
+				Rcnt++;
+				if (hole == make_pair(nRy, nRx)) {
+					is_R_hole = true;
+					break;
+				}
+			}
+			else break;
+		}
+		//파란 구슬 굴리기
+		while (board[By + dy[k]][Bx + dx[k]]) {
+			if (By + dy[k] >= 0 && By + dy[k] < N && Bx + dx[k] >= 0 && Bx + dx[k] < M) {
+				nBx += dx[k], nRy += dy[k];
+				Bcnt++;
+				if (hole == make_pair(nBy, nBx)) {
+					is_B_hole = true;
+					break;
+				}
+			}
+			else break;
+		}
+
+		//공이 구슬에 도착했을 경우 처리
+		if (is_R_hole) {
+			//둘 다 도착했을 경우
+			if (is_B_hole) {
+				//빨간 공이 먼저 도착하면 됨
+				if (Rcnt < Bcnt) answer = cnt;
+				else answer = -1;
+			}
+			else answer = cnt;
+			break;
+		}
+		//빨간 구슬과 파란 구슬이 동일한 위치에 있을때 처리하기
+		if (nRx == nBx && nRy == nBy) {
+			//세로 동일선상
+			if (Rx == Bx) {
+				if (Ry > By) nRy -= dy[k];
+				else nBy -= dy[k];
+			}
+			//가로 동일선상
+			else {
+				if (Rx > By) nRx -= dx[k];
+				else nBx -= dx[k];
+			}
+		}
+		//DFS
+		DFS(nRx, nRy, nBx, nBy, cnt);
+	}
+}
+
+void solution() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	//Input
+	cin >> N >> M;
+	// 0 : 벽  // 1 : 공간 , 구멍
+	// 빨간공과 파란공, 구멍은 위치 저장
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			char temp;
+			cin >> temp;
+			if (temp == '#') board[i][j] = 0;
+			else {
+				board[i][j] = 1;
+				if (temp == 'R')
+					red = make_pair(i, j);
+				else if (temp == 'B')
+					blue = make_pair(i, j);
+				else if (temp == 'O')
+					hole = make_pair(i, j);
+			}
+		}
+	}
+
+	//DFS
+	DFS(red.second, red.first, blue.second, blue.first, 0);
+	cout << answer;
+}
+
+/*
+	기본 아이디어
+	1. 백트래킹 : 왼쪽으로 가본 길이 안 맞으면 원상 복귀해서 다시 원래대로
+	2. dfs
+	3. 빨강 & 파랑 구슬 동시에 움직이기 (?)
+	일단은 이렇게 하고, 최솟값은 나중에 생각해보자
+*/
