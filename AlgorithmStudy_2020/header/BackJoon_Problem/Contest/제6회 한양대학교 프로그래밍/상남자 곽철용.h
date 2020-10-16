d44655c0837629ef13ab0ca92152dba4ef555012
DonghyunKim98 <akainoo@naver.com>
2020-10-16 10:33:29 +0900

    10.16 : 제 6회 한양대학교 프로그래밍 경시대회

--- AlgorithmStudy_2020/header/BackJoon_Problem/Contest/제6회 한양대학교 프로그래밍/상남자 곽철용.h
+++ AlgorithmStudy_2020/header/BackJoon_Problem/Contest/제6회 한양대학교 프로그래밍/상남자 곽철용.h
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
 //17947번 
 using namespace std;
+typedef pair<int, int> pii;
+
 void solution() {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
 	int N, M, K;
 	cin >> N >> M >> K;
 	vector<int> vc(K);
+	int cnt = 4 * N / K;
 	for (int i = 0; i < K; i++) {
-		int cnt = 4 * N / K;
 		//나누어 떨어지지 않는 경우 예외 처리
 		if (i != 0 && i <= (4 * N) % K) cnt++;
 		vc[i] = cnt;
@@ -20,54 +21,35 @@ void solution() {
 	}
 	int num1, num2;
 	cin >> num1 >> num2;
-	int gap = abs(num1 % K - num2 % K);
 	vc[num1 % K]--, vc[num2 & K]--;
+	int gap = abs(num1 % K - num2 % K);
 
-	//오름차순 정렬을 위한 입력
-	vector<int> vc2;
-	for (int i = 0; i < vc.size(); i++) {
-		int cnt = vc[i];
-		while (cnt > 0) {
-			vc2.push_back(i);
-			cnt--;
-		}
+	//내림차순 정렬을 위한 입력
+	vector<pii> vc2;
+	for (int i = vc.size()-1; i >= 0; i--) {
+		vc2.push_back({ i,vc[i] });
 	}
-
 	int answer = 0;
 	int start = 0, end = 1;
-	int len = vc2.size() - 1;
-	//아무리 pair가 많아도 최대 M-1개. 
 	/*
-		0 0 0 1 1 2 3 4
-		(0,1) (0,1) (0,2), (3,4)
+		4 3 2 1 1 0 0 0
 	*/
-	map<int, bool> mp;
-	while (end<=len&&answer<M-1) {
-		//곽철용보다 점수가 높을때까지 일단 end index를 올려줌
-		while (vc2[end] - vc2[start] <= gap) {
+	while (1) {
+		// 곽철용보다 gap보다 작음
+		if (vc2[start].first - vc2[end].first <= gap) {
 			end++;
-			if (end > len) break;
 		}
-		if (end >len) break;
-		/*
-			알고리즘 포인트!!! => 사람들이 뽑는 것은 조합이지 중복조합이 아니다.
-			따라서 start-end가 gap을 넘는 순간의 pari를 발견했으면 그 2개를 더이상 못쓰므로,
-			start와 end를 둘다 증가시켜줘야 한다.
-		*/
-		//이전에 end가 방문한 곳이 아니라면 -> answer
-		if (mp.count(start)==0) {
-			mp[end] = true;
-			answer++,start++,end++;
-		}
-		//이전에 end가 방문한 곳이라면 -> start만 하나 늘려줌
+		// 곽철용보다 gap이 큼
 		else {
-			start++;
+			int MIN = min(vc2[start].second, vc2[end].second);
+			answer += MIN;
+			vc2[start].second -= MIN, vc2[end].second -= MIN;
+			if (vc2[start].second == 0) start++;
+			if (vc2[end].second == 0) end++;
 		}
+		// 오른쪽 포인터가 끝까지 옴 -> 탈출
+		if (end == vc2.size()) break;
 	}
+	answer = min(answer, M - 1);
 	cout << answer;
 }
-/*
-	two pointer method 이용
-	=> O(n log n)이 걸림 (Sorting => n long n / pointer => n)
-	=> 2개의 포인터를 이용해서 한 배열을 중복해서 2번돌지 않고 각각 처음부터 끝까지 2*N으로 돈다는 것에 있다.
-*/
