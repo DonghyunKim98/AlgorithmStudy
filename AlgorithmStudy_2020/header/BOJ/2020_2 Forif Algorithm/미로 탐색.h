87955180914bf120735cbc1aa3e314d971dec7b0
DongHyunKim <akainoo123@gmail.com>
2024-01-30 21:45:26 +0900

    feat : main cpp 만들기

--- AlgorithmStudy_2020/header/BOJ/2020_2 Forif Algorithm/미로 탐색.h
+++ AlgorithmStudy_2020/header/BOJ/2020_2 Forif Algorithm/미로 탐색.h
@@ -7,43 +7,54 @@ int N, M;
 int MAP[MAX][MAX];
 int visited[MAX][MAX];
 pii dir[4] = {
-	{0,1},{1,0},{-1,0},{0,-1}
-};
+	{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
 
-int BFS(int ypos, int xpos) {
+int BFS(int ypos, int xpos)
+{
 	queue<pii> q;
 	memset(visited, 0, sizeof(visited));
-	q.push({ ypos,xpos });
+	q.push({ypos, xpos});
 	visited[ypos][xpos] = 1;
-	while (!q.empty()) {
+	while (!q.empty())
+	{
 		pii curPos = q.front();
 		q.pop();
 		int cnt = visited[curPos.first][curPos.second];
-		if (curPos.first == N-1 && curPos.second == M-1) {
+		if (curPos.first == N - 1 && curPos.second == M - 1)
+		{
 			return cnt;
 		}
-		for (int k = 0; k < 4; k++) {
+		for (int k = 0; k < 4; k++)
+		{
 			int ny = curPos.first + dir[k].first;
 			int nx = curPos.second + dir[k].second;
-			if (0 <= ny && ny < N && 0 <= nx && nx < M) {
-				if (MAP[ny][nx] == 0) continue;
-				if (visited[ny][nx] < cnt + 1) {
+			if (0 <= ny && ny < N && 0 <= nx && nx < M)
+			{
+				if (MAP[ny][nx] == 0)
+					continue;
+				if (visited[ny][nx] < cnt + 1)
+				{
 					visited[ny][nx] = cnt + 1;
-					q.push({ ny,nx });
+					q.push({ny, nx});
 				}
 			}
 		}
 	}
 }
-void solution() {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+void solution()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 	cin >> N >> M;
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++)
+	{
 		string temp;
 		cin >> temp;
-		for (int j = 0; j < M; j++) {
+		for (int j = 0; j < M; j++)
+		{
 			MAP[i][j] = int(temp[j] - '0');
 		}
 	}
-	cout<< BFS(0, 0);
+	cout << BFS(0, 0);
 }
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+
+using namespace std;
+
+int CNT = 10;
+
+int main()
+{
+    int a = 3;
+
+    a += 5;
+    a *= 2;
+    a -= CNT;
+
+    cout << a << endl;
+}
